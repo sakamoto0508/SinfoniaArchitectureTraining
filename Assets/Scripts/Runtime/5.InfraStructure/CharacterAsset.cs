1fcbcb600c4c170742911972c820cdb2dad467ea
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-04-07 07:09:05 +0900

    [add]chacterasset

---
+++ Assets/Scripts/Runtime/5.InfraStructure/CharacterAsset.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace InfraStructure
+{
+    /// <summary>
+    ///     キャラクターの基本的なデータを保持するScriptableObject。
+    /// </summary>
+    [CreateAssetMenu(fileName = "NewCharacterAsset", menuName = "Character/Asset")]
+    public class CharacterAsset : ScriptableObject
+    {
+        public string CharacterName => _characterName;
+        public GameObject Character => _character;
+        public float Health => _health;
+        public float Defence => _defence;
+        public float MoveSpeed => _moveSpeed;
+        public float AttackPower => _attackPower;
+        public float AttackRange => _attackRange;
+        public float AttackSpeed => _attackSpeed;
+        public float CriticalChance => _criticalChance;
+        public float CriticalDamage => _criticalDamage;
+
+        [SerializeField] private string _characterName;
+        [SerializeField] private GameObject _character;
+        [SerializeField] private float _health;
+        [SerializeField] private float _defence;
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _attackPower;
+        [SerializeField] private float _attackRange;
+        [SerializeField] private float _attackSpeed;
+        [SerializeField] private float _criticalChance;
+        [SerializeField] private float _criticalDamage;
+    }
+}
