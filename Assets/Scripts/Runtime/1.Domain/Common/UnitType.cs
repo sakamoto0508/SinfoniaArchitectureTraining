d884e5b00fa6ceeb04976b9c9bb82f4576f1cf46
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-04-08 05:52:36 +0900

    [add]unittype

---
+++ Assets/Scripts/Runtime/1.Domain/Common/UnitType.cs
@@ -0,0 +1,12 @@
+namespace Domain
+{
+    /// <summary>
+    ///     ユニットの種別を表す列挙型。
+    /// </summary>
+    public enum UnitType
+    {
+        Fighter,
+        Archer,
+        Warrior,
+    }
+}
